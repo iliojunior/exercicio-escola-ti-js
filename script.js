65f13f8a6b5a4a60e6e333a4d1b0fb1896f61d50
Ilio Junior <ilioadriano@live.com>
2017-02-25 19:43:39 -0300

    Documentacao de todos os metodos e variaveis da aplicacao

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
+/**
+ * Constante da opcao de sair
+ * @type {string}
+ */
 const OPCAO_SAIR = "!SAIR";
+
+/**
+ * Constante da opcao de saber a distancia
+ * @type {string}
+ */
 const OPCAO_DISTANCIA = "!DISTANCIA";
+
+/**
+ * Array geral(mapa), com as cidades cadastradas pelo usuario
+ * @type {Array}
+ */
 minhasCidades = [];
 
-iniciarAplicacao();
-
+/**
+ * Mostra a distancia entre duas cidades
+ * @param {string} cidadeOrigem - Nome da Cidade de Origem informada pelo usuario
+ * @param {string} cidadeDestino - Nome da Cidade de Destino informada pelo usuario
+ * @returns {void}
+ */
 function mostrarDistancia(cidadeOrigem, cidadeDestino) {
     var indexCidade = minhasCidades.indexOfCidade(cidadeOrigem);
     var indexDistancia = minhasCidades[indexCidade].distancias.indexOfDistancia(cidadeDestino);
@@ -12,12 +30,26 @@ function mostrarDistancia(cidadeOrigem, cidadeDestino) {
     alert(mensagem);
 }
 
+/**
+ * Pergunta as cidades de origem e de Destino para exibir
+ * @returns {void}
+ */
 function calcularDistancia() {
+    //Pede a Cidade de Origem
     var cidadeOrigem = prompt("Digite a cidade de origem");
+
+    //Pede a Cidade de Destino
     var cidadeDestino = prompt("Digite a cidade de destino");
+
+    //Exibe ao usuario a distancia entre as cidades informadas
     mostrarDistancia(cidadeOrigem, cidadeDestino);
 }
 
+/**
+ * Funcao inicial da aplicacao
+ * Permanece nela enquanto o usuario desejar utilizar a aplicacao
+ * @returns {void}
+ */
 function iniciarAplicacao() {
     var nomeNovaCidade;
 
@@ -47,10 +79,22 @@ function iniciarAplicacao() {
     console.log(convertoToJson(minhasCidades));
 }
 
+/**
+ * Cria as distancias da cidade cadastrada para as demais cidades.
+ * @param {string} nomeCidade
+ * @param {number} distancia
+ * @returns {{cidade: string, distancia: number}}
+ */
 function criarDistancia(nomeCidade, distancia) {
     return {cidade: nomeCidade, distancia: distancia};
 }
 
+/**
+ * Cria uma nova cidade solicitada pelo usuario
+ * @param {string} nomeCidade - Recebe o nome da cidade para criar
+ * @param {array} distancias - Recebe distancias para cadastrar nesta cidade, caso nao seja informada o padrao eh []
+ * @returns {{nome: string, distancias: Array}}
+ */
 function criarCidade(nomeCidade, distancias) {
     distancias = distancias || [];
 
@@ -63,49 +107,91 @@ function criarCidade(nomeCidade, distancias) {
     return novaCidade;
 }
 
-function percorrerCidadePerguntandoDistancia(novaCidadeNome) {
+/**
+ * Adiciona uma nova cidade ao mapa.
+ * Pede ao usuario para informar a distancia da nova cidade para as demais
+ * @param {string} novaCidadeNome -> Nome da nova cidade
+ * @returns {void}
+ */
+function adicionarCidade(novaCidadeNome) {
+    /**
+     * Clona o mapa global para uma variavel local
+     */
     var cidadeParaPerguntar = minhasCidades.cloneCidade();
 
+    /**
+     * Cria a nova cidade
+     * @type {{nome: string, distancias: Array}}
+     */
     var novaCidade = criarCidade(novaCidadeNome);
-    if (cidadeParaPerguntar.isEmpty()) {
 
+    /**
+     * Instancia da cidade caso o mapa esteja vazio
+     */
+    if (cidadeParaPerguntar.isEmpty()) {
         minhasCidades.push(novaCidade);
-        return;
     }
 
+    /**
+     * Perguntar as distancias da nova cidade para as demais
+     */
     for (var i = 0; !cidadeParaPerguntar.isEmpty(); i++) {
+        /**
+         * @type {number} - Distancia entre a nova cidade e as demais cidades
+         */
         var distancia = 0;
+
+        /**
+         * @type {array} - Pilha de cidades para perguntas as distancias
+         */
         var cidadeAtualParaPerguntar = cidadeParaPerguntar[0];
 
-        //Entra em um laço enquanto o usuario não digitar a distancia em numero
+        /**
+         * Entra em um laço enquanto o usuario não digitar a distancia em numero
+         */
         do {
             var entradaUsuario = prompt("Digite uma distancia valida de \"" + novaCidadeNome + "\" para \"" + cidadeAtualParaPerguntar.nome + "\".");
         } while (parseFloat(entradaUsuario) == NaN);
 
-        //Converte a entrada do usuario em um numero
+        /**
+         * @type {Number} Converte a entrada do usuario em um numero
+         */
         distancia = parseFloat(entradaUsuario);
 
-        //Cria distancia desta cidade para outra cidade
+        /**
+         * @type {{cidade: string, distancia: number}} Cria distancia desta cidade para outra cidade
+         */
         var distanciaDestaParaOutraCidade = criarDistancia(cidadeAtualParaPerguntar.nome, distancia);
 
-        //Cria distancia da outra cidade para esta cidade
+
+        /**
+         * @type {{cidade: string, distancia: number}} - Cria distancia da outra cidade para esta cidade
+         */
         var distanciaDaOutraParaEstaCidade = criarDistancia(novaCidadeNome, distancia);
 
-        //Altera a distancia da outra cidade para esta
+        /**
+         * Altera a distancia da outra cidade para esta
+         */
         minhasCidades[i].distancias.push(distanciaDaOutraParaEstaCidade);
 
-        //Adiciona uma nova distancia na nova Cidade
+        /**
+         * Adiciona uma nova distancia na nova Cidade
+         */
         novaCidade.distancias.push(distanciaDestaParaOutraCidade);
 
-        //Remove da fila
+        /**
+         * Remove da fila
+         */
         cidadeParaPerguntar.shift();
     }
 
-    //Adiciona a nova cidade no mapa
+    /**
+     * Adiciona a nova cidade no mapa
+     */
     minhasCidades.push(novaCidade);
 }
 
-function adicionarCidade(nomeDaCidade) {
-    percorrerCidadePerguntandoDistancia(nomeDaCidade);
-}
-
+/**
+ * Inicia a aplicacao
+ */
+iniciarAplicacao();
